38e6e7c36fabdd258aa3ab2c12b349bc427a47ec
cecil <cecilghimire@gmail.com>
2022-09-28 20:24:29 +0545

    side bar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import './SideBar.css'
-
+import {LineStyle,Timeline,TrendingUp}from '@material-ui/icons';
 export default function SideBar() {
   return (
     <div className='sideBar'>
-        SIDEBAR
+       <div className="sideBarWrapper">
+           <div className="sideBarMenu">
+            <h4 className="sideBarTitle">
+                DashBoard
+            </h4>
+            <ul className="sideBarList">
+                <li className="sideBarItem">
+                    <LineStyle/>Home
+                </li>
+                <li className="sideBarItem">
+                    <Timeline/>Analytics
+                </li>
+                <li className="sideBarItem">
+                    <TrendingUp/>Sales
+                </li>
+            </ul>
+           </div>
+
+
+           <div className="sideBarMenu">
+            <h4 className="sideBarTitle">
+                DashBoard
+            </h4>
+            <ul className="sideBarList">
+                <li className="sideBarItem">
+                    <LineStyle/>Home
+                </li>
+                <li className="sideBarItem">
+                    <Timeline/>Analytics
+                </li>
+                <li className="sideBarItem">
+                    <TrendingUp/>Sales
+                </li>
+            </ul>
+           </div>
+       </div>
     </div>
   )
 }
